8f7e55bb03ca778f42996532f5ce1f2332d68ba4
Yusufhan Kircova <ykircova@gmail.com>
2023-08-15 05:26:26 +0300

    Init

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// JavaScript for scrolling to sections
+function scrollToSection(sectionId) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      window.scrollTo({
+        top: element.offsetTop,
+        behavior: 'smooth',
+      });
+    }
+  }
